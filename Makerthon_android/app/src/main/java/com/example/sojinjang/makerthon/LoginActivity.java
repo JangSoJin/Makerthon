04bad21654f812febdf8e978875fd1b9e05db98d
Lagnesia <sjkwon619@naver.com>
2017-03-04 07:14:42 +0900

    Login-Register 01

---
+++ Makerthon_android/app/src/main/java/com/example/sojinjang/makerthon/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.example.sojinjang.makerthon;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    Button regist;
+    ImageButton kakao;
+    ImageButton facebook;
+    private RelativeLayout mlayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mlayout = (RelativeLayout) findViewById(R.id.activity_main);
+        mlayout.setBackgroundColor(Color.rgb(148,210,238));
+
+        login = (Button) findViewById(R.id.Login);
+        regist = (Button) findViewById(R.id.Regist);
+        login.setBackgroundColor(Color.rgb(-215,109,156));
+        regist.setBackgroundColor(Color.rgb(-215,109,156));
+
+        kakao.setBackgroundColor(Color.rgb(148,210,238));
+        facebook.setBackgroundColor(Color.rgb(148,210,238));
+
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),RegistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
